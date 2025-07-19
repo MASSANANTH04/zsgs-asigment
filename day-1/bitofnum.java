c03ddadb392efecf72e49921c0fa944f075f8106
MASSANANTH04 <asananth04@gmail.com>
2025-07-19 16:37:30 +0530

    ji

--- day-1/bitofnum.java
+++ day-1/bitofnum.java
@@ -1,13 +1,6 @@
 public class bitofnum {
     public static void main(String arg[]){
-        int a=18;
-        int binary=0;
-        while(a>0){
-            binary=(a%2)+binary;
-            a=a/2;
-        }
-        System.err.println("hello world");
-        System.out.println(binary);
+      
     }
     
 }
